55ed7625fec48a31eebba37d8b07575fee52c62d
Samat Karymshakov <bskordo@gmail.com>
2017-01-07 19:31:23 +0600

    Seems ok

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,41 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+from datetime import datetime, timedelta
+from pytz import timezone
+import pytz
 
-def get_midnighters():
-    pass
+
+utc = pytz.utc
+format_time = '%H:%M:%S'
+start_midnight ='00:00:00'
+end_midnight = '06:00:00'
+
+def load_attempts():
+    payload = {'page':0}
+    inf_store = []
+    for page in range(1,11):
+        payload['page'] += page
+        inf_from_url = requests.get('http://devman.org/api/challenges/solution_attempts/', params=payload).json()
+        inf_store.extend(inf_from_url['records'])
+        payload['page'] = 0
+    return inf_store
+
+def get_midnighter(user):
+    if not user['timestamp']:
+        return False
+    user_time = (utc.localize(datetime.utcfromtimestamp(user['timestamp']))).strftime(format_time)
+    if start_midnight < user_time < end_midnight:
+        return True
+    else:
+        return False
+
+
+
+
 
 if __name__ == '__main__':
-  pass
+    attempts = load_attempts()
+    midnighters = [user['username'] for user in attempts if get_midnighter(user)]
+    print("Midnighters are:")
+    for user in midnighters:
+        print(user)
+    
