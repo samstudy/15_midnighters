75f3a2b3022f9fef859e1aba12232a914ad48cdb
Samat Karymshakov <bskordo@gmail.com>
2017-01-08 16:07:16 +0600

    Modified boolen function and changed payload param.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -9,14 +9,12 @@ END_MIDNIGHT = 6
 
 
 def load_attempts():
-    payload = {'page':1}
-    count_pages = requests.get('https://devman.org/api/challenges/solution_attempts/',params=payload).json()
+    count_pages = requests.get('https://devman.org/api/challenges/solution_attempts/',params={'page':1}).json()
     pages = count_pages['number_of_pages'] 
     inf_store = []
     for page in range(pages):
-        inf_from_url = requests.get('https://devman.org/api/challenges/solution_attempts/', params=payload).json()
+        inf_from_url = requests.get('https://devman.org/api/challenges/solution_attempts/', params={'page':page+1}).json()
         inf_store.extend(inf_from_url['records'])
-        payload['page'] += 1
     return inf_store
 
 
@@ -24,9 +22,8 @@ def is_midnighter(user):
     if not user['timestamp']:
         return False
     user_time = ((pytz.utc).localize(datetime.utcfromtimestamp(user['timestamp']))).hour
-    if  user_time > END_MIDNIGHT:
-        return False
-    return True
+    return  user_time < END_MIDNIGHT
+        
 
 
 if __name__ == '__main__':
